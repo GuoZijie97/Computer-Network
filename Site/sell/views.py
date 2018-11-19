a6fb4486f43016b4da643b6f919c101812976932
ArielLi1220 <limx.15@sem.tsinghua.edu.cn>
2018-11-19 12:18:47 +0800

    新增了分类商品展示页面的url和view

--- Site/sell/views.py
+++ Site/sell/views.py
@@ -1,9 +1,21 @@
 
 # Create your views here.
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404, render
+from .models import goods, order
 
 
 def index(request):
     context = {}
     return render(request, 'sell/index.html', context)
+
+def subcategory_view(request, goods_subcategory):
+    goods_list = get_list_or_404(goods, goods_subcategory=goods_subcategory)
+    first_goods = get_object_or_404(goods, goods_subcategory=goods_subcategory)
+    context = {
+        'goods_list': goods_list,
+        'first_goods': first_goods,
+    }
+    return render(request, 'sell/subcategory_goods_list.html', context)
+
+

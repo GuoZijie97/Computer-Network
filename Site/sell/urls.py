a51a1117a73688baec0a1815c1d68b56329f0b31
ArielLi1220 <limx.15@sem.tsinghua.edu.cn>
2018-11-24 15:22:04 +0800

    修改url保证左右箭头可以加载出来

--- Site/sell/urls.py
+++ Site/sell/urls.py
@@ -7,8 +7,12 @@ Created on Sun Oct 28 20:17:17 2018
 """
 
 from django.urls import path
+from django.conf.urls import url
+from django.views.static import serve
 
 from . import views
+from shesite.settings import BASE_DIR
+import os
 
 app_name = 'sell'
 urlpatterns = [
@@ -21,7 +25,8 @@ urlpatterns = [
     # ex: /sell/1/subcategory/
     path('<str:goods_subcategory>/subcategory/', views.subcategory_view, name='subcategory_view'),
     # ex: /sell/5/single-product-details/
-    path('<int:pk>/single-product-details', views.detail, name='detail'),
+    path('<int:pk>/single-product-details/', views.detail, name='detail'),
+    url(r'^[0-9]*/single-product-details/(?P<path>.*)$', serve, {"document_root": BASE_DIR}),
     # ex: /sell/is_selling/
     path('is_selling/', views.is_selling, name='is_selling'),
 ]
--- Site/shesite/urls.py
+++ Site/shesite/urls.py
@@ -18,10 +18,12 @@ from django.contrib import admin
 from django.urls import include,path
 from django.views.static import serve
 from .settings import MEDIA_ROOT
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('sell/', include('sell.urls')),
     path('admin/', admin.site.urls),
     url(r'^accounts/', include('users.urls')),
     url(r'^media/(?P<path>.*)$', serve, {"document_root":MEDIA_ROOT}),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
